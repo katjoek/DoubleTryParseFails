1aa1ae3ee49a9a81717cb32a3fb9cbac70e12085
Mark van de Veerdonk <github@mvandeveerdonk.dds.nl>
2024-04-03 13:16:48 +0200

    Added Repro-100397 project

---
+++ Repro-100397/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var options = new JsonSerializerOptions
+{
+    WriteIndented = true,
+    ReferenceHandler = ReferenceHandler.IgnoreCycles,
+};
+var jsonString = JsonSerializer.Serialize(CultureInfo.CurrentCulture, options);
+Console.WriteLine(jsonString);
+
+var str = "2,000.000";
+var success = double.TryParse(str, NumberStyles.Any, CultureInfo.CurrentCulture, out var value);
+Console.WriteLine($"success: {success}");
+Console.WriteLine($"value: {value}");
